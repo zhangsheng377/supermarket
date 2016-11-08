785455fd201a7182a1653b97259fdfd0fe41f153
张胜东 <435878393@qq.com>
2016-11-08 17:02:13 +0800

    换成window builder来做界面

--- java/src/supermarket/Login.java
+++ java/src/supermarket/Login.java
@@ -1,12 +1,79 @@
 package supermarket;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 @SuppressWarnings("serial")
-public class Login extends Supermarket{
-	@SuppressWarnings("deprecation")
-	public Login(){
-		super();
-		this.resize(800, 600);
-		this.getContentPane().setLayout(null);
-		this.setTitle("Login");
+public class Login extends Supermarket {
+
+	private JPanel contentPane;
+	private JTextField textField_1;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(3, 2));
+		setTitle("Login");
+		
+		JLabel lblNewLabel = new JLabel("\u7528\u6237\u540D");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblNewLabel);
+		
+		textField_1 = new JTextField();
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("\u5BC6\u7801");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblNewLabel_1);
+		
+		passwordField = new JPasswordField();
+		contentPane.add(passwordField);
+		
+		JButton btnNewButton = new JButton("\u767B\u5F55");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("\u9000\u51FA");
+		contentPane.add(btnNewButton_1);
+	}
+
 }
